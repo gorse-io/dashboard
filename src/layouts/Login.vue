f0b793e6a3f419fc38da187ad04ebf27437ef421
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2022-02-11 10:31:50 +0800

    notify login fail

--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -10,6 +10,10 @@
             <d-form-input id="username" name="user_name" class="mb-1" placeholder="Username" />
             <label class="sr-only" for="password">Password</label>
             <d-form-input id="password" name="password" class="mt-2" type="password" placeholder="Password" />
+            <d-alert v-if="this.$route.query.msg === 'incorrect'" theme="danger" class="mt-2"
+                     :show="timeUntilDismissed"
+                     @alert-dismissed="timeUntilDismissed = 0"
+                     @alert-dismiss-countdown="handleTimeChange">The user name or password is incorrect.</d-alert>
             <d-button class="mt-2" theme="primary" type="submit">Login</d-button>
           </d-form>
         </d-card-body>
@@ -24,3 +28,18 @@
   max-width: 480px;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      timeUntilDismissed: 5,
+    };
+  },
+  methods: {
+    handleTimeChange(time) {
+      this.timeUntilDismissed = time;
+    },
+  },
+};
+</script>
