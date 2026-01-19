2b8528cd6d104a202105d7b0aa5d9ec35fe7d724
zhenghaoz <zhangzhenghao@hotmail.com>
2026-01-19 12:25:39 +0000

    Refactor Chat component by removing unused context and item ID fields

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -8,35 +8,7 @@
     </div>
 
     <d-row>
-      <d-col lg="4" md="12" sm="12" class="mb-4">
-        <div class="card card-small mb-4">
-          <div class="card-header border-bottom">
-            <h6 class="m-0">Context</h6>
-          </div>
-          <div class="card-body p-0 pb-2">
-            <d-list-group flush>
-              <d-list-group-item class="p-3">
-                <d-row>
-                  <d-col>
-                    <d-input-group prepend="Item ID" class="mb-4">
-                      <d-input v-model="item_id" @keyup.enter.native="search_item" />
-                      <d-input-group-addon append>
-                        <d-button class="btn-white" @click="search_item"><i class="material-icons">search</i></d-button>
-                      </d-input-group-addon>
-                    </d-input-group>
-                  </d-col>
-                </d-row>
-                <d-row>
-                  <d-col>
-                    <highlightjs language='json' :code="json" />
-                  </d-col>
-                </d-row>
-              </d-list-group-item>
-            </d-list-group>
-          </div>
-        </div>
-      </d-col>
-      <d-col lg="8" md="12" sm="12" class="mb-4">
+      <d-col lg="12" md="12" sm="12" class="mb-4">
         <div class="card card-small mb-4">
           <div class="card-header border-bottom">
             <h6 class="m-0">Prompt</h6>
@@ -51,14 +23,7 @@
                   </d-col>
                 </d-row>
                 <d-row>
-                  <d-col sm="12" md="10">
-                    <d-input-group prepend="Template" class="mb-3">
-                      <d-select>
-                        <option :value="null"></option>
-                      </d-select>
-                    </d-input-group>
-                  </d-col>
-                  <d-col sm="12" md="2" class="text-right">
+                  <d-col sm="12" md="12" class="text-right">
                     <d-button outline @click="send">Send</d-button>
                   </d-col>
                 </d-row>
@@ -88,16 +53,13 @@
 <script>
 import axios from 'axios';
 import nunjucks from 'nunjucks';
-import utils from '../utils';
 
 export default {
   data() {
     return {
       prompt: '',
       message: '',
-      item_id: '',
       context: {},
-      json: '{}',
     };
   },
   methods: {
@@ -127,18 +89,6 @@ export default {
       }).catch((error) => {
         this.message = error;
       });
-    },
-    search_item() {
-      axios({
-        method: 'get',
-        url: `/api/item/${this.item_id}`,
-      })
-        .then((response) => {
-          this.context = {
-            item: response.data,
-          };
-          this.json = utils.stringify(this.context);
-        });
     },
     render_prompt(event) {
       this.prompt = nunjucks.renderString(event, this.context);
