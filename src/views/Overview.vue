29544e35af5ab216dd877dce646a0fb357c6e7b4
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2026-06-13 19:43:29 +0800

    Fix small stats placeholder in mobile

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -3,7 +3,6 @@
     <!-- Page Header -->
     <d-row no-gutters class="page-header py-4">
       <d-col col sm="4" class="text-center text-sm-left mb-4 mb-sm-0">
-        <span class="text-uppercase page-subtitle">Dashboard</span>
         <h3 class="page-title">Overview</h3>
       </d-col>
     </d-row>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,7 +4,6 @@
     <!-- Page Header -->
     <div class="page-header row no-gutters py-4">
       <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
-        <span class="text-uppercase page-subtitle">Overview</span>
         <h3 class="page-title">Settings</h3>
       </div>
     </div>
